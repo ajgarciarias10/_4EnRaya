bc9e4f937ca2217867ea902eea8df7d5b556e153
ajgarciarias10 <ajgarciarias@gmail.com>
2023-05-05 19:11:15 +0200

    MinMaxRestringido

--- src/plot4/MiniMaxRestrainedPlayer.java
+++ src/plot4/MiniMaxRestrainedPlayer.java
@@ -31,7 +31,6 @@ public class MiniMaxRestrainedPlayer extends Player {
      */
     @Override
     public int turno(Grid tablero, int conecta) {
-
         int posicion = getRandomColumn(tablero);
         // to do
 
@@ -39,5 +38,62 @@ public class MiniMaxRestrainedPlayer extends Player {
         return posicion;
 
     } // turno
+    private int heuristica(int filas, int columnas,int[][] tablero,int conecta){
+        int ganador=0;
+        int ganar1;
+        int ganar2;
+        boolean salir = false;
+        //Mira check win
+        //Ver horizontal
+        // Comprobar horizontal
+        for (int i = 0; (i < filas) && !salir; i++) {
+            ganar1 = 0;
+            ganar2 = 0;
+            for (int j = 0; (j < columnas) && !salir; j++) {
+                if (tablero[i][j] != Main.VACIO) {
+                    //Conteo 1 y 2
+                    if (tablero[i][j] == Main.PLAYER1) {
+                        ganar1++;
+                    } else {
+                        ganar1 = 0;
+                    }
 
-} // MiniMaxRestrainedPlayer
+                    if (tablero[i][j] == Main.PLAYER2) {
+                        ganar2++;
+                    } else {
+                        ganar2 = 0;
+                    }
+                    //Comprobacion de si supera el limete establecido
+
+                    //Comprueba fichas contiguas
+                }
+            }
+        return 0;
+        }
+        /**
+         // Gana el jugador 1
+         if (ganar1 >= conecta) {
+         ganador = Main.PLAYER1;
+         // Ganador 1 en horizontal;
+         salir = true;
+         }
+         **/
+        // if (!salir) {
+
+        /**
+         // Gana el jugador 2
+         if (ganar2 >= conecta) {
+         ganador = Main.PLAYER2;
+         // Ganador 2 en horizontal;
+         salir = true;
+         }
+
+         // }
+         } else {
+         ganar1 = 0;
+         ganar2 = 0;
+         }
+         **/
+
+        return ganador;
+    } // MiniMaxRestrainedPlayer
